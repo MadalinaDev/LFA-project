881223ab9994f5aba03250881a81f74db27efd4c
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-02-16 20:43:23 +0200

    implemented Grammar class: constructor and generate_string method

--- Lab-1/main.py
+++ Lab-1/main.py
@@ -1,1 +1,20 @@
-print("initial setup for python")
+class Grammar:
+    def __init__(self):
+        self.VN = {'S', 'I', 'J', 'K'}
+        self.VT = {'a', 'b', 'c', 'e', 'n', 'f', 'm'}
+        self.P = {
+            'S': ['cI'],
+            'I': ['bJ', 'fI', 'eK', 'e'],
+            'J': ['nJ', 'cS'],
+            'K': ['nK', 'm']
+        }
+        self.S = 'S'
+
+    def generate_string(self):
+        def expand(symbol):
+            if symbol in self.VT:
+                return symbol
+            else:
+                production = random.choice(self.P[symbol])
+                return ''.join(expand(s) for s in production)
+        return expand(self.S)

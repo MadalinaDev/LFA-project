1797e28c45ec51907e415af4f90a7fd699aa0941
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-02-16 21:11:35 +0200

    added finite automaton with constructor and build_transitions method

--- Lab-1/main.py
+++ Lab-1/main.py
@@ -16,5 +16,37 @@ class Grammar:
                 return symbol
             else:
                 production = random.choice(self.P[symbol])
-                return ''.join(expand(s) for s in production)
+                expanded_results = []
+                for sym in production:
+                    expanded_results.append(expand(sym))
+                return ''.join(expanded_results)
         return expand(self.S)
+
+
+
+class FiniteAutomaton:
+    def __init__(self, grammar):
+        self.states = grammar.VN.union({'F'})
+        self.alphabet = grammar.VT
+        
+        self.transitions = self.build_transitions(grammar)
+        
+        self.start_state = grammar.S
+        self.final_states = {'F'}
+
+
+def build_transitions(self, grammar):
+    transitions = {}
+    for state in grammar.VN:
+        transitions[state] = {}
+    
+        for production in grammar.P[state]:
+            # it has in a single terminal symbol
+            if len(production) == 1 and production[0] in grammar.VT:
+                transitions[state][production[0]] = 'F'
+            
+            # it has a terminal symbol + a non-terminal symbol
+            elif len(production) == 2:
+                # we are for sure that it's a right-linear grammar 
+                transitions[state][production[0]] = production[1]
+    return transitions
